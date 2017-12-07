aeb4bb85040173633840acfce8631855e323e935
Thomas O'Donnell <thomas.odonnell@izettle.com>
2017-12-07 11:58:22 +0000

    Add option to limit concurrent helm calls
    Have added a new argument `--max-connections` that can be used to set a
    limit on the number of concurrent calls to helm at one time. To enable
    this I have switched `SyncCharts` from using a WaitGroup to using a pool
    of workers and a queue of jobs. To ensure that this is thread safe and
    an attempt is made to sync each chart at the end.
    
    Fixes https://github.com/roboll/helmfile/issues/23

--- state/state.go
+++ state/state.go
@@ -124,33 +124,65 @@ func (state *HelmState) SyncRepos(helm helmexec.Interface) []error {
 	return nil
 }
 
-func (state *HelmState) SyncCharts(helm helmexec.Interface, additonalValues []string) []error {
-	var wg sync.WaitGroup
+func (state *HelmState) SyncCharts(helm helmexec.Interface, additonalValues []string, workerLimit int) []error {
 	errs := []error{}
+	jobQueue := make(chan ChartSpec)
+	doneQueue := make(chan bool)
+	errQueue := make(chan error)
 
-	for _, chart := range state.Charts {
-		wg.Add(1)
-		go func(wg *sync.WaitGroup, chart ChartSpec) {
-			flags, flagsErr := flagsForChart(state.BaseChartPath, &chart)
-			if flagsErr != nil {
-				errs = append(errs, flagsErr)
-			}
-			for _, value := range additonalValues {
-				valfile, err := filepath.Abs(value)
-				if err != nil {
-					errs = append(errs, err)
-				}
-				flags = append(flags, "--values", valfile)
-			}
-			if len(errs) == 0 {
-				if err := helm.SyncChart(chart.Name, normalizeChart(state.BaseChartPath, chart.Chart), flags...); err != nil {
-					errs = append(errs, err)
-				}
-			}
-			wg.Done()
-		}(&wg, chart)
+	if workerLimit < 1 {
+		workerLimit = len(state.Charts)
 	}
-	wg.Wait()
+
+	for w := 1; w <= workerLimit; w++ {
+		go func() {
+			for chart := range jobQueue {
+
+				flags, flagsErr := flagsForChart(state.BaseChartPath, &chart)
+				if flagsErr != nil {
+					errQueue <- flagsErr
+					doneQueue <- true
+					continue
+				}
+
+				haveValueErr := false
+				for _, value := range additonalValues {
+					valfile, err := filepath.Abs(value)
+					if err != nil {
+						errQueue <- err
+						haveValueErr = true
+					}
+					flags = append(flags, "--values", valfile)
+				}
+
+				if haveValueErr {
+					doneQueue <- true
+					continue
+				}
+
+				if err := helm.SyncChart(chart.Name, normalizeChart(state.BaseChartPath, chart.Chart), flags...); err != nil {
+					errQueue <- err
+				}
+				doneQueue <- true
+			}
+		}()
+	}
+
+	go func() {
+		for _, chart := range state.Charts {
+			jobQueue <- chart
+		}
+		close(jobQueue)
+	}()
+
+	for i := 0; i < len(state.Charts); {
+		select {
+		case err := <-errQueue:
+			errs = append(errs, err)
+		case <-doneQueue:
+			i++
+		}
+	}
 
 	if len(errs) != 0 {
 		return errs
