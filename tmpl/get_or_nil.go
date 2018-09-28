f3fde1aea19ab56e0a0d3c96660fa79d951ef3d3
Yusuke KUOKA <ykuoka@gmail.com>
2018-09-28 11:38:15 +0900

    add more tests adn fixes bug in accessing struct fields

--- tmpl/get_or_nil.go
+++ tmpl/get_or_nil.go
@@ -14,32 +14,32 @@ func (e *noValueError) Error() string {
 	return e.msg
 }
 
-func get(path string, o interface{}) (interface{}, error) {
+func get(path string, obj interface{}) (interface{}, error) {
 	if path == "" {
-		return o, nil
+		return obj, nil
 	}
 	keys := strings.Split(path, ".")
-	switch oo := o.(type) {
+	switch typedObj := obj.(type) {
 	case map[string]interface{}:
-		v, ok := oo[keys[0]]
+		v, ok := typedObj[keys[0]]
 		if !ok {
-			return nil, &noValueError{fmt.Sprintf("no value exist for key \"%s\" in %v", keys[0], oo)}
+			return nil, &noValueError{fmt.Sprintf("no value exist for key \"%s\" in %v", keys[0], typedObj)}
 		}
 		return get(strings.Join(keys[1:], "."), v)
 	case map[interface{}]interface{}:
-		v, ok := oo[keys[0]]
+		v, ok := typedObj[keys[0]]
 		if !ok {
-			return nil, &noValueError{fmt.Sprintf("no value exist for key \"%s\" in %v", keys[0], oo)}
+			return nil, &noValueError{fmt.Sprintf("no value exist for key \"%s\" in %v", keys[0], typedObj)}
 		}
 		return get(strings.Join(keys[1:], "."), v)
 	default:
-		maybeStruct := reflect.ValueOf(oo).Elem()
+		maybeStruct := reflect.ValueOf(typedObj)
 		if maybeStruct.NumField() < 1 {
-			return nil, &noValueError{fmt.Sprintf("unexpected type(%v) of value for key \"%s\": it must be either map[string]interface{} or any struct", reflect.TypeOf(o), keys[0])}
+			return nil, &noValueError{fmt.Sprintf("unexpected type(%v) of value for key \"%s\": it must be either map[string]interface{} or any struct", reflect.TypeOf(obj), keys[0])}
 		}
 		f := maybeStruct.FieldByName(keys[0])
 		if !f.IsValid() {
-			return nil, &noValueError{fmt.Sprintf("no field named \"%s\" exist in %v", keys[0], oo)}
+			return nil, &noValueError{fmt.Sprintf("no field named \"%s\" exist in %v", keys[0], typedObj)}
 		}
 		v := f.Interface()
 		return get(strings.Join(keys[1:], "."), v)
