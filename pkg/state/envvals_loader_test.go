4c2512fbaf1b53f93ff9ab79a10f72777d6e1c93
Kevin Jing Qiu <kevin@idempotent.ca>
2020-06-04 16:39:38 -0400

    Make linter happy

--- pkg/state/envvals_loader_test.go
+++ pkg/state/envvals_loader_test.go
@@ -25,8 +25,8 @@ func newLoader() *EnvironmentValuesLoader {
 	}
 
 	readFile := func(s string) ([]byte, error) { return []byte{}, nil }
-	dirExists := func(d string) bool {return false}
-	fileExists := func(f string) bool {return false}
+	dirExists := func(d string) bool { return false }
+	fileExists := func(f string) bool { return false }
 	return NewEnvironmentValuesLoader(storage, ioutil.ReadFile, sugar, remote.NewRemote(sugar, "/tmp", readFile, dirExists, fileExists))
 }
 
