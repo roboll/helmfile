5ac0ae533d775771d6d543d9058d6ba152cae170
ento <ento.entotto@gmail.com>
2020-06-04 07:13:15 -0800

    Parse resources with kustomize to compare them structure by structure

--- test/local-helm-diff.go
+++ test/local-helm-diff.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
-	"github.com/databus23/helm-diff/diff"
-	"github.com/databus23/helm-diff/manifest"
+	"github.com/go-test/deep"
 	"github.com/spf13/cobra"
+	"sigs.k8s.io/kustomize/kyaml/kio"
 	"sigs.k8s.io/kustomize/kyaml/sets"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+type diffSource string
+
+const (
+	diffSourceLeft  diffSource = "left"
+	diffSourceRight diffSource = "right"
 )
 
 var (
@@ -57,19 +65,51 @@ func run(cmd *cobra.Command, args []string) {
 
 	inBoth := leftYamls.Intersection(rightYamls)
 	for _, f := range inBoth.List() {
-		leftManifest, err := readManifest(filepath.Join(left, f))
+		leftPath := filepath.Join(left, f)
+		leftNodes, err := readManifest(leftPath)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v\n", err)
 			os.Exit(1)
 		}
-		rightManifest, err := readManifest(filepath.Join(right, f))
+		rightPath := filepath.Join(right, f)
+		rightNodes, err := readManifest(rightPath)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v\n", err)
 			os.Exit(1)
 		}
-		hasChanges := diff.Manifests(leftManifest, rightManifest, nil, false, 5, os.Stderr)
-		if hasChanges {
-			exitCode = 1
+		ps := pairs{}
+		for _, node := range leftNodes {
+			if err := ps.add(node, diffSourceLeft); err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+		}
+		for _, node := range rightNodes {
+			if err := ps.add(node, diffSourceRight); err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+		}
+		for _, p := range ps.list {
+			switch {
+			case p.left != nil && p.right == nil:
+				fmt.Fprintf(os.Stderr, "Only in %s: %s\n", leftPath, nodeToString(p.left))
+				exitCode = 1
+			case p.left == nil && p.right != nil:
+				fmt.Fprintf(os.Stderr, "Only in %s: %s\n", rightPath, nodeToString(p.right))
+				exitCode = 1
+			default:
+				diff := deep.Equal(p.left, p.right)
+				if diff != nil {
+					id := nodeToString(p.left)
+					fmt.Fprintf(os.Stderr, "--- %s %s", id, leftPath)
+					fmt.Fprintf(os.Stderr, "+++ %s %s", id, rightPath)
+					for _, d := range diff {
+						fmt.Fprintf(os.Stderr, d)
+					}
+					exitCode = 1
+				}
+			}
 		}
 	}
 
@@ -88,12 +128,117 @@ func globYamlFilenames(dir string) (sets.String, error) {
 	return set, nil
 }
 
-func readManifest(path string) (map[string]*manifest.MappingResult, error) {
-	bytes, err := ioutil.ReadFile(path)
+func readManifest(path string) ([]*yaml.RNode, error) {
+	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
-	return manifest.Parse(string(bytes), "default"), nil
+	defer f.Close()
+	rr := &kio.ByteReader{
+		DisableUnwrapping:     true,
+		Reader:                f,
+		OmitReaderAnnotations: true,
+	}
+	nodes, err := rr.Read()
+	if err != nil {
+		return nil, err
+	}
+	for i := range nodes {
+		node := nodes[i].YNode()
+		node.HeadComment = ""
+	}
+	return nodes, nil
+}
+
+func nodeToString(node *yaml.RNode) string {
+	meta, err := node.GetMeta()
+	if err != nil {
+		return fmt.Sprintf("%v", node)
+	}
+	ns := meta.Namespace
+	if ns == "" {
+		ns = "~X"
+	}
+	nm := meta.Name
+	if nm == "" {
+		nm = "~N"
+	}
+	gv := meta.APIVersion
+	if gv == "" {
+		gv = "~G_~V"
+	}
+	k := meta.Kind
+	if k == "" {
+		k = "~K"
+	}
+	gvk := strings.Join([]string{gv, k}, "_")
+	return strings.Join([]string{gvk, ns, nm}, "|")
+
+}
+
+// lifted from kustomize/kyaml/kio/filters/merge3.go
+type pairs struct {
+	list []*pair
+}
+
+func (ps *pairs) isSameResource(meta1, meta2 yaml.ResourceMeta) bool {
+	if meta1.Name != meta2.Name {
+		return false
+	}
+	if meta1.Namespace != meta2.Namespace {
+		return false
+	}
+	if meta1.APIVersion != meta2.APIVersion {
+		return false
+	}
+	if meta1.Kind != meta2.Kind {
+		return false
+	}
+	return true
+}
+
+func (ps *pairs) add(node *yaml.RNode, source diffSource) error {
+	nodeMeta, err := node.GetMeta()
+	if err != nil {
+		return err
+	}
+	for i := range ps.list {
+		p := ps.list[i]
+		if ps.isSameResource(p.meta, nodeMeta) {
+			return p.add(node, source)
+		}
+	}
+	p := &pair{meta: nodeMeta}
+	if err := p.add(node, source); err != nil {
+		return err
+	}
+	ps.list = append(ps.list, p)
+	return nil
+}
+
+type pair struct {
+	meta  yaml.ResourceMeta
+	left  *yaml.RNode
+	right *yaml.RNode
+}
+
+func (p *pair) add(node *yaml.RNode, source diffSource) error {
+	switch source {
+	case diffSourceLeft:
+		if p.left != nil {
+			return fmt.Errorf("left source already specified")
+		}
+		p.left = node
+	case diffSourceRight:
+		if p.right != nil {
+			return fmt.Errorf("right source already specified")
+		}
+		p.right = node
+	default:
+		return fmt.Errorf("unknown diff source value: %s", source)
+	}
+	return nil
+
 }
 
 func main() {
