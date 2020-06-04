65557a9ce55b0e08e49745845807f9746c0b8e02
ento <ento.entotto@gmail.com>
2020-06-04 12:57:26 -0800

    Decode resources into plain maps
    The RNode type from kustomize uses yaml.Node under the hood,
    which carries extra information like line numbers, which
    become noisy when comparing with deep.Equal.

--- test/local-helm-diff.go
+++ test/local-helm-diff.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/go-test/deep"
 	"github.com/spf13/cobra"
-	"sigs.k8s.io/kustomize/kyaml/kio"
-	"sigs.k8s.io/kustomize/kyaml/sets"
-	"sigs.k8s.io/kustomize/kyaml/yaml"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
 type diffSource string
@@ -93,19 +93,19 @@ func run(cmd *cobra.Command, args []string) {
 		for _, p := range ps.list {
 			switch {
 			case p.left != nil && p.right == nil:
-				fmt.Fprintf(os.Stderr, "Only in %s: %s\n", leftPath, nodeToString(p.left))
+				fmt.Fprintf(os.Stderr, "Only in %s: %s\n", leftPath, p.left.getId())
 				exitCode = 1
 			case p.left == nil && p.right != nil:
-				fmt.Fprintf(os.Stderr, "Only in %s: %s\n", rightPath, nodeToString(p.right))
+				fmt.Fprintf(os.Stderr, "Only in %s: %s\n", rightPath, p.right.getId())
 				exitCode = 1
 			default:
 				diff := deep.Equal(p.left, p.right)
 				if diff != nil {
-					id := nodeToString(p.left)
-					fmt.Fprintf(os.Stderr, "--- %s %s", id, leftPath)
-					fmt.Fprintf(os.Stderr, "+++ %s %s", id, rightPath)
+					id := p.left.getId()
+					fmt.Fprintf(os.Stderr, "< %s %s\n", id, leftPath)
+					fmt.Fprintf(os.Stderr, "> %s %s\n", id, rightPath)
 					for _, d := range diff {
-						fmt.Fprintf(os.Stderr, d)
+						fmt.Fprintf(os.Stderr, "%s\n", d)
 					}
 					exitCode = 1
 				}
@@ -128,46 +128,75 @@ func globYamlFilenames(dir string) (sets.String, error) {
 	return set, nil
 }
 
-func readManifest(path string) ([]*yaml.RNode, error) {
+type resource map[string]interface{}
+
+type meta struct {
+	apiVersion string
+	kind       string
+	name       string
+	namespace  string
+}
+
+func (res resource) getMeta() (meta, error) {
+	if len(res) == 0 {
+		return meta{}, nil
+	}
+	m := meta{}
+	apiVersion, _ := res["apiVersion"].(string)
+	m.apiVersion = apiVersion
+	kind, _ := res["kind"].(string)
+	m.kind = kind
+	metadata, _ := res["metadata"].(map[string]interface{})
+	name, _ := metadata["name"].(string)
+	m.name = name
+	namespace, _ := metadata["namespace"].(string)
+	m.namespace = namespace
+	return m, nil
+}
+
+func readManifest(path string) ([]resource, error) {
+	var err error
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
-	rr := &kio.ByteReader{
-		DisableUnwrapping:     true,
-		Reader:                f,
-		OmitReaderAnnotations: true,
+	decoder := yaml.NewYAMLToJSONDecoder(f)
+	resources := []resource{}
+	for {
+		r := make(resource)
+		err = decoder.Decode(&r)
+		if err != nil {
+			break
+		}
+		if len(r) > 0 {
+			resources = append(resources, r)
+		}
 	}
-	nodes, err := rr.Read()
-	if err != nil {
+	if err != nil && err != io.EOF {
 		return nil, err
 	}
-	for i := range nodes {
-		node := nodes[i].YNode()
-		node.HeadComment = ""
-	}
-	return nodes, nil
+	return resources, nil
 }
 
-func nodeToString(node *yaml.RNode) string {
-	meta, err := node.GetMeta()
+func (res resource) getId() string {
+	meta, err := res.getMeta()
 	if err != nil {
-		return fmt.Sprintf("%v", node)
+		return fmt.Sprintf("%v", res)
 	}
-	ns := meta.Namespace
+	ns := meta.namespace
 	if ns == "" {
 		ns = "~X"
 	}
-	nm := meta.Name
+	nm := meta.name
 	if nm == "" {
 		nm = "~N"
 	}
-	gv := meta.APIVersion
+	gv := meta.apiVersion
 	if gv == "" {
 		gv = "~G_~V"
 	}
-	k := meta.Kind
+	k := meta.kind
 	if k == "" {
 		k = "~K"
 	}
@@ -181,24 +210,24 @@ type pairs struct {
 	list []*pair
 }
 
-func (ps *pairs) isSameResource(meta1, meta2 yaml.ResourceMeta) bool {
-	if meta1.Name != meta2.Name {
+func (ps *pairs) isSameResource(meta1, meta2 meta) bool {
+	if meta1.name != meta2.name {
 		return false
 	}
-	if meta1.Namespace != meta2.Namespace {
+	if meta1.namespace != meta2.namespace {
 		return false
 	}
-	if meta1.APIVersion != meta2.APIVersion {
+	if meta1.apiVersion != meta2.apiVersion {
 		return false
 	}
-	if meta1.Kind != meta2.Kind {
+	if meta1.kind != meta2.kind {
 		return false
 	}
 	return true
 }
 
-func (ps *pairs) add(node *yaml.RNode, source diffSource) error {
-	nodeMeta, err := node.GetMeta()
+func (ps *pairs) add(node resource, source diffSource) error {
+	nodeMeta, err := node.getMeta()
 	if err != nil {
 		return err
 	}
@@ -217,12 +246,12 @@ func (ps *pairs) add(node *yaml.RNode, source diffSource) error {
 }
 
 type pair struct {
-	meta  yaml.ResourceMeta
-	left  *yaml.RNode
-	right *yaml.RNode
+	meta  meta
+	left  resource
+	right resource
 }
 
-func (p *pair) add(node *yaml.RNode, source diffSource) error {
+func (p *pair) add(node resource, source diffSource) error {
 	switch source {
 	case diffSourceLeft:
 		if p.left != nil {
