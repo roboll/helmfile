roboll/helmfile

commit b4857937fbcbdb52865e7d78074775d187ad7716

Fix lint to not fail for duplicate release names across ns (#1388)
Fixes #1384

pkg/state/state.go
@@ -856,12 +856,28 @@ func (st *HelmState) PrepareCharts(helm helmexec.Interface, dir string, concurre
 					chartPath = normalizeChart(st.basePath, release.Chart)
 				} else {
 					fetchFlags := []string{}
-					if release.Version != "" {
-						chartPath = path.Join(dir, release.Name, release.Version, release.Chart)
-						fetchFlags = append(fetchFlags, "--version", release.Version)
-					} else {
-						chartPath = path.Join(dir, release.Name, "latest", release.Chart)
+
+					pathElems := []string{
+						dir,
 					}
+
+					if release.TillerNamespace != "" {
+						pathElems = append(pathElems, release.TillerNamespace)
+					}
+
+					if release.Namespace != "" {
+						pathElems = append(pathElems, release.Namespace)
+					}
+
+					chartVersion := "latest"
+					if release.Version != "" {
+						chartVersion = release.Version
+						fetchFlags = append(fetchFlags, "--version", release.Version)
+					}
+
+					pathElems = append(pathElems, chartVersion, release.Chart)
+
+					chartPath = path.Join(pathElems...)
 
 					if st.isDevelopment(release) {
 						fetchFlags = append(fetchFlags, "--devel")
@@ -875,6 +891,7 @@ func (st *HelmState) PrepareCharts(helm helmexec.Interface, dir string, concurre
 							return
 						}
 					}
+
 					// Set chartPath to be the path containing Chart.yaml, if found
 					fullChartPath, err := findChartDirectory(chartPath)
 					if err == nil {

commit 193f31058612c6b6ed85f4ac54e74fdaa999336c

Fix regression on template failing due to duplicate fetch target
This fixes a regression introduced in #1388
Fixes ##1394

pkg/state/state.go
@@ -890,7 +890,7 @@ func (st *HelmState) PrepareCharts(helm helmexec.Interface, dir string, concurre
 						fetchFlags = append(fetchFlags, "--version", release.Version)
 					}
 
-					pathElems = append(pathElems, chartVersion, release.Chart)
+					pathElems = append(pathElems, release.Name, release.Chart, chartVersion)
 
 					chartPath = path.Join(pathElems...)
 