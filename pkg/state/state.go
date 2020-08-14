roboll/helmfile

commit 61b61d30092acc4dbbfb29a4ed1ca089c94fca85

Fix `helmfile template` with Helm 3 ignoring chart version (#1406)
This fixes a regression introduced in #1399
Fixes #1377

pkg/state/state.go
@@ -905,8 +905,6 @@ func (st *HelmState) PrepareCharts(helm helmexec.Interface, dir string, concurre
 						}
 					}
 				} else {
-					fetchFlags := []string{}
-
 					pathElems := []string{
 						dir,
 					}
@@ -922,19 +920,15 @@ func (st *HelmState) PrepareCharts(helm helmexec.Interface, dir string, concurre
 					chartVersion := "latest"
 					if release.Version != "" {
 						chartVersion = release.Version
-						fetchFlags = append(fetchFlags, "--version", release.Version)
 					}
 
 					pathElems = append(pathElems, release.Name, chartName, chartVersion)
 
 					chartPath = path.Join(pathElems...)
 
-					if st.isDevelopment(release) {
-						fetchFlags = append(fetchFlags, "--devel")
-					}
-
 					// only fetch chart if it is not already fetched
 					if _, err := os.Stat(chartPath); os.IsNotExist(err) {
+						fetchFlags := st.chartVersionFlags(release)
 						fetchFlags = append(fetchFlags, "--untar", "--untardir", chartPath)
 						if err := helm.Fetch(chartName, fetchFlags...); err != nil {
 							results <- &downloadResults{err: err}
@@ -1774,14 +1768,7 @@ func (st *HelmState) timeoutFlags(helm helmexec.Interface, release *ReleaseSpec)
 }
 
 func (st *HelmState) flagsForUpgrade(helm helmexec.Interface, release *ReleaseSpec, workerIndex int) ([]string, []string, error) {
-	flags := []string{}
-	if release.Version != "" {
-		flags = append(flags, "--version", release.Version)
-	}
-
-	if st.isDevelopment(release) {
-		flags = append(flags, "--devel")
-	}
+	flags := st.chartVersionFlags(release)
 
 	if release.Verify != nil && *release.Verify || release.Verify == nil && st.HelmDefaults.Verify {
 		flags = append(flags, "--verify")
@@ -1840,7 +1827,7 @@ func (st *HelmState) flagsForUpgrade(helm helmexec.Interface, release *ReleaseSp
 }
 
 func (st *HelmState) flagsForTemplate(helm helmexec.Interface, release *ReleaseSpec, workerIndex int) ([]string, []string, error) {
-	flags := []string{}
+	flags := st.chartVersionFlags(release)
 
 	var err error
 	flags, err = st.appendHelmXFlags(flags, release)
@@ -1858,14 +1845,7 @@ func (st *HelmState) flagsForTemplate(helm helmexec.Interface, release *ReleaseS
 }
 
 func (st *HelmState) flagsForDiff(helm helmexec.Interface, release *ReleaseSpec, workerIndex int) ([]string, []string, error) {
-	flags := []string{}
-	if release.Version != "" {
-		flags = append(flags, "--version", release.Version)
-	}
-
-	if st.isDevelopment(release) {
-		flags = append(flags, "--devel")
-	}
+	flags := st.chartVersionFlags(release)
 
 	disableOpenAPIValidation := false
 	if release.DisableOpenAPIValidation != nil {
@@ -1902,6 +1882,20 @@ func (st *HelmState) flagsForDiff(helm helmexec.Interface, release *ReleaseSpec,
 		return nil, files, err
 	}
 	return append(flags, common...), files, nil
+}
+
+func (st *HelmState) chartVersionFlags(release *ReleaseSpec) []string {
+	flags := []string{}
+
+	if release.Version != "" {
+		flags = append(flags, "--version", release.Version)
+	}
+
+	if st.isDevelopment(release) {
+		flags = append(flags, "--devel")
+	}
+
+	return flags
 }
 
 func (st *HelmState) appendApiVersionsFlags(flags []string) []string {

commit d6c05bbefc93393695c94b46d3beee632c9c5e11

Fix regression for helmfile template with helm v2 since v0.125.4
This fixes the regression introduced by #1406 that resulted in `helmfile template` with helm v2 failing when there is any release with `version`.

Fixes #1414

pkg/state/state.go
@@ -939,8 +939,13 @@ func (st *HelmState) PrepareCharts(helm helmexec.Interface, dir string, concurre
 					// 1. It is a local chart and we can use it in later process (helm upgrade/template/lint/etc)
 					//    without any modification, or
 					// 2. It is a remote chart which can be safely handed over to helm,
-					//    because the version of Helm used in this transaction support downloading the chart instead,
-					//    and we don't need any modification to the chart
+					//    because the version of Helm used in this transaction (helm v3 or greater) support downloading
+					//    the chart instead, AND we don't need any modification to the chart
+					//
+					//    Also see HelmState.chartVersionFlags(). For `helmfile template`, it's called before `helm template`
+					//    only on helm v3.
+					//    For helm 2, we `helm fetch` with the version flags and call `helm template`
+					//    WITHOUT the version flags.
 				} else {
 					pathElems := []string{
 						dir,
@@ -1873,7 +1878,16 @@ func (st *HelmState) flagsForUpgrade(helm helmexec.Interface, release *ReleaseSp
 }
 
 func (st *HelmState) flagsForTemplate(helm helmexec.Interface, release *ReleaseSpec, workerIndex int) ([]string, []string, error) {
-	flags := st.chartVersionFlags(release)
+	var flags []string
+
+	// `helm template` in helm v2 does not support `--version` flag. So we fetch with the version flag and then template
+	// without the flag. See PrepareCharts function to see the Helmfile implementation of chart fetching.
+	//
+	// `helm template` in helm v3 supports `--version` and it automatically fetches the remote chart to template,
+	// so we skip fetching on helmfile-side and let helm fetch it.
+	if helm.IsHelm3() {
+		flags = st.chartVersionFlags(release)
+	}
 
 	var err error
 	flags, err = st.appendHelmXFlags(flags, release)