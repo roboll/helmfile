d60ab5a16cc9e3803222dcca5bf57c07e8dc97d5
Andrey Tuzhilin <andrey@3adigital.ru>
2021-03-16 11:09:58 +0300

    fix: fixed secrets decryption failed issue

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -138,17 +138,24 @@ if [[ helm_major_version -eq 3 ]]; then
   sops="sops --hc-vault-transit $VAULT_ADDR/v1/sops/keys/key"
   mkdir -p ${dir}/tmp
 
-  test_start "secretssops"
-
   info "Encrypt secrets"
   ${sops} -e ${dir}/env-1.secrets.yaml > ${dir}/tmp/env-1.secrets.sops.yaml || fail "${sops} failed at ${dir}/env-1.secrets.yaml"
   ${sops} -e ${dir}/env-2.secrets.yaml > ${dir}/tmp/env-2.secrets.sops.yaml || fail "${sops} failed at ${dir}/env-2.secrets.yaml"
+
+  test_start "secretssops.1 - should fail without secrets plugin"
 
   info "Ensure helm-secrets is not installed"
   ${helm} plugin rm secrets || true
 
   info "Ensure helmfile fails when no helm-secrets is installed"
-  ${helmfile} -f ${dir}/secretssops.yaml -e direct build; code="$?"; echo Code: "$code"; [ "${code}" -ne 0 ] || fail "\"helmfile build\" should fail without secrets plugin"
+  unset code
+  ${helmfile} -f ${dir}/secretssops.yaml -e direct build || code="$?"; code="${code:-0}"
+  echo Code: "${code}"
+  [ "${code}" -ne 0 ] || fail "\"helmfile build\" should fail without secrets plugin"
+
+  test_pass "secretssops.1"
+
+  test_start "secretssops.2 - should succeed with secrets plugin"
 
   info "Ensure helm-secrets is installed"
   ${helm} plugin install https://github.com/jkroepke/helm-secrets --version v3.5.0
@@ -156,7 +163,7 @@ if [[ helm_major_version -eq 3 ]]; then
   info "Ensure helmfile succeed when helm-secrets is installed"
   ${helmfile} -f ${dir}/secretssops.yaml -e direct build || fail "\"helmfile build\" shouldn't fail"
 
-  test_pass "secretssops"
+  test_pass "secretssops.2"
 fi
 
 # ALL DONE -----------------------------------------------------------------------------------------------------------
