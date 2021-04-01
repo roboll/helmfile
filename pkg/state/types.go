1748cbfcea98123c2c223281d4ae911bde3a2534
Quan TRAN <itscaro@users.noreply.github.com>
2021-04-01 02:47:12 +0200

    Add .StateValues as alias of .Values in releaseTemplateDataRelease (#1744)
    Ref #1743

--- pkg/state/types.go
+++ pkg/state/types.go
@@ -28,7 +28,8 @@ type releaseTemplateData struct {
 	// It contains a subset of ReleaseSpec that is known to be useful to dynamically render values.
 	Release releaseTemplateDataRelease
 	// Values is accessible as `.Values` and it contains default state values overrode by environment values and override values.
-	Values map[string]interface{}
+	Values      map[string]interface{}
+	StateValues *map[string]interface{}
 	// Namespace is HelmState.OverrideNamespace.
 	// You should better use Release.Namespace as it might work as you'd expect even if OverrideNamespace is not set.
 	// See releaseTemplateDataRelease.Namespace for more information.
