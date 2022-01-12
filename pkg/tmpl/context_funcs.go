7d773aba8a895fbdd9d4c3d7606d3ce5392cbad1
Quan TRAN <itscaro@users.noreply.github.com>
2022-01-12 23:55:29 +0100

    Update context_funcs.go

--- pkg/tmpl/context_funcs.go
+++ pkg/tmpl/context_funcs.go
@@ -20,7 +20,7 @@ type Values = map[string]interface{}
 func (c *Context) createFuncMap() template.FuncMap {
 	funcMap := template.FuncMap{
 		"exec":             c.Exec,
-		"isFile":           IsFile,
+		"isFile":           c.IsFile,
 		"readFile":         c.ReadFile,
 		"readDir":          ReadDir,
 		"toYaml":           ToYaml,
@@ -118,15 +118,22 @@ func (c *Context) Exec(command string, args []interface{}, inputs ...string) (st
 	return string(bytes), nil
 }
 
-func IsFile(filename string) (bool, error) {
-    stat, err := os.Stat(filename)
-    if err == nil {
-        return !stat.IsDir(), nil
-    }
-    if errors.Is(err, os.ErrNotExist) {
-        return false, nil
-    }
-    return false, err
+func (c *Context) IsFile(filename string) (bool, error) {
+	var path string
+	if filepath.IsAbs(filename) {
+		path = filename
+	} else {
+		path = filepath.Join(c.basePath, filename)
+	}
+
+	stat, err := os.Stat(path)
+	if err == nil {
+		return !stat.IsDir(), nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
 }
 
 func (c *Context) ReadFile(filename string) (string, error) {
