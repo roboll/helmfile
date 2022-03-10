dbc40f68b8486f7c180080cb7c6c0978d75d51e1
Yusuke Kuoka <ykuoka@gmail.com>
2022-03-10 18:42:55 +0900

    Make release template to work on `needs` (#2099)
    It turned out that Helmfile has never had support for release template on `needs`.
    This adds that, along with the new end-to-end test suite to verify helmfile template output with snapshot testing involving a real `helmfile build` command.
    
    Ref #2098

---
+++ test/e2e/template/helmfile/snapshot_test.go
@@ -0,0 +1,50 @@
+package helmfile
+
+import (
+	"context"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHelmfileTemplateWithBuildCommand(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	projectRoot := filepath.Join(filepath.Dir(filename), "..", "..", "..", "..")
+	helmfileBin := filepath.Join(projectRoot, "helmfile")
+	testdataDir := "testdata"
+
+	entries, err := os.ReadDir(testdataDir)
+	require.NoError(t, err)
+
+	for _, e := range entries {
+		if !e.IsDir() {
+			t.Fatalf("Unexpected type of entry at %s", e.Name())
+		}
+
+		name := e.Name()
+
+		t.Run(name, func(t *testing.T) {
+			inputFile := filepath.Join(testdataDir, name, "input.yaml")
+
+			want, err := os.ReadFile(filepath.Join(testdataDir, name, "output.yaml"))
+			require.NoError(t, err)
+
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			cmd := exec.CommandContext(ctx, helmfileBin, "-f", inputFile, "build")
+			got, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Logf("%s", string(got))
+			}
+			require.NoError(t, err)
+
+			require.Equal(t, string(want), string(got))
+		})
+	}
+}
